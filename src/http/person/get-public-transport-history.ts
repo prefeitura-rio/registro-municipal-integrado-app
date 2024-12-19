fdb5ae9513c376674fcef9df906bb5ee1a52dcf2
Victor Assis <victor_assis@poli.ufrj.br>
2024-12-19 15:43:18 -0300

    fix: :bug: Aplica limite máximo de pontos de viagens na tela de Transporte (50)

--- src/http/person/get-public-transport-history.ts
+++ src/http/person/get-public-transport-history.ts
@@ -5,9 +5,11 @@ export async function getPublicTransportHistory(cpf: string) {
   const response = await api.get<PublicTransportEvent[]>(
     `/person/${cpf}/public-transport/history`,
   )
-  return response.data.sort(
-    (a, b) =>
-      new Date(b.datetime_transacao).getTime() -
-      new Date(a.datetime_transacao).getTime(),
-  )
+  return response.data
+    .sort(
+      (a, b) =>
+        new Date(b.datetime_transacao).getTime() -
+        new Date(a.datetime_transacao).getTime(),
+    )
+    .slice(0, 50)
 }
